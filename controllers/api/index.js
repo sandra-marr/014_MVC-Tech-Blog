ed357ed9772030a7d03d0d368e34757b28012593
SPeats <srpeters44@gmail.com>
2021-06-25 10:52:43 -0700

    removed comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes.js');
 const postRoutes = require('./postRoutes.js');
-const commentRoutes = require('./commentRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/post', postRoutes);
-router.use('/comments', commentRoutes);
 
 module.exports = router;
