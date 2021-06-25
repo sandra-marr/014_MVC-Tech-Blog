df83714e9c3a48d195244bf2be2abcc44944f2b7
SPeats <srpeters44@gmail.com>
2021-06-24 18:52:00 -0700

    adding models

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+        },
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts',
+  }
+);
+
+module.exports = Post;
