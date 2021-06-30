084a4ca67e33ac35a45712f127120d5fa62ca877
SPeats <srpeters44@gmail.com>
2021-06-29 21:44:01 -0700

    fixing connection

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-}).catch( (err)=> {
-console.log("Could not connect to the server.")
 });
+
+// .catch( (err)=> {
+//   console.log("Could not connect to the server.")
+//   })
